23a38727b7c1aaac8eb029f538c42c6010899dd9
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2022-07-07 14:34:11 -0300

    Added basic fetch to test how it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState("");
+    const [logo, setLogo] = useState(null);
+    const [description, setDescription] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch(
+                    "https://nutri-dev-a8f9a-default-rtdb.firebaseio.com/client/6cb80ab9-f4dd-4a18-adb9-70250683c2f6.json"
+                );
+                const data = await response.json();
+
+                setTitle(data.title);
+                setLogo(data.logo);
+                setDescription(data.description);
+            } catch (error) {
+                setHasError(true);
+                console.error("fetch error", error);
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 2000);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <p>Aguarde...</p>;
+    }
+
+    return (
+        <div className="App">
+            {hasError ? (
+                <p>Erro ao buscar os dados do servidor.</p>
+            ) : (
+                <header className="App-header">
+                    {logo && <img src={logo} alt="logo" />}
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </header>
+            )}
+        </div>
+    );
 }
 
 export default App;
